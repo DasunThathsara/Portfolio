924055206bd0b8897644a273482d7f5b669e51fa
Dasun Thathsara <81586965+DasunThathsara@users.noreply.github.com>
2022-04-26 15:36:51 +0530

    Adding Skills Section

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 const header = document.querySelector("header");
 
+const first_skill = document.querySelector(".skill:first-child");
+const sk_counter = document.querySelectorAll(".counter span");
+const progress_bar = document.querySelectorAll(".skills svg circle");
+
+window.addEventListener("scroll", () => {
+    if(!skillsPlayed) skillsCounter();
+});
+
 /*----------------------- Sticky Navbar -----------------------*/
 
 function stickyNavbar(){
@@ -16,5 +24,47 @@ let sr = ScrollReveal({
     distance: "60px",
 });
 
-sr.reveal(".showcase-info", { delay : 600 });
-sr.reveal(".showcase-image", { origin : "top", delay : 700 });
+sr.reveal(".anmd", { delay : 600 });
+sr.reveal(".anmt", { origin : "top", delay : 700 });
+sr.reveal(".anms", { origin : "top", delay : 200 });
+
+/*----------------------- Skills Progress Bar Animation -----------------------*/
+
+function hasReached(el){
+    let topPosition = el.getBoundingClientRect().top;
+    
+    if(window.innerHeight >= topPosition + el.offsetHeight) return true;
+    return false;
+}
+
+function updateCount(num, maxNum){
+    let currentNum = +num.innerText;
+
+    if(currentNum < maxNum){
+        num.innerText = currentNum + 1;
+        setTimeout(() => {
+            updateCount(num, maxNum)
+        }, 25);
+    }
+}
+
+let skillsPlayed = false;
+
+function skillsCounter(){
+    if(!hasReached(first_skill)) return;
+
+    skillsPlayed = true;
+
+    sk_counter.forEach((counter, i) => {
+        let target = +counter.dataset.target;
+        let strokeValue = 427 - 427 * (target / 100);
+
+        progress_bar[i].style.setProperty("--target", strokeValue);
+
+        setTimeout(() => {
+            updateCount(counter, target);
+        }, 400);
+    });
+
+    progress_bar.forEach((p) => (p.style.animation = "progress 3s ease-in-out forwards"));
+}
