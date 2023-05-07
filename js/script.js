052bb3018cfe54c274af43e4f1ef85f60f508b36
Dasun Thathsara <dasun.thathsara.sri@gmail.com>
2023-05-07 18:53:14 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,6 +7,11 @@ const progress_bar = document.querySelectorAll(".skills svg circle");
 const ml_section = document.querySelector(".milestones");
 const ml_counters = document.querySelectorAll(".number span");
 
+
+const toggle_btn = document.querySelector(".toggle_btn");
+
+const hamburger = document.querySelector(".hamburger");
+
 window.addEventListener("scroll", () => {
     if(!skillsPlayed) skillsCounter();
     if(!mlPlayed) mlCounter();
@@ -43,6 +48,7 @@ sr.reveal(".anmd", { delay : 600 });
 sr.reveal(".anmt", { origin : "top", delay : 700 });
 sr.reveal(".anms", { origin : "top", delay : 200 });
 sr.reveal(".anml", { origin : "left", delay : 700 });
+sr.reveal(".anmr", { origin : "right", delay : 700 });
 
 /*----------------------- Skills Progress Bar Animation -----------------------*/
 
@@ -105,14 +111,57 @@ function mlCounter(){
 /*-------------------------------- Dark Mode -------------------------------*/
 
 function myFunction() {
-    var element1 = document.body;
-    element1.classList.toggle("dark");
+    var element = document.body;
+    element.classList.toggle("dark");
 
-    var element2 = document.querySelector('.uil-moon');
-    element2.classList.toggle("uil-sun");
+    element = document.querySelector('.uil-moon');
+    element.classList.toggle("uil-sun");
 
-    var element3 = document.querySelector('.nav-link');
-    element3.classList.toggle(".nav-link-dark");
-    
+    element = document.querySelector('.heading');
+    element.classList.toggle("heading-dark");
+
+    element = document.querySelector('.sub-heading');
+    element.classList.toggle("sub-heading-dark");
+
+    element = document.querySelector('.about');
+    element.classList.toggle("about-dark");
+
+    element = document.querySelector('.skills');
+    element.classList.toggle("skills-dark");
+
+    element = document.querySelector('.services');
+    element.classList.toggle("services-dark");
+
+    element = document.querySelector('.skills-box');
+    element.classList.toggle("skills-box-dark");
+
+    element = document.querySelector('.srv-card');
+    element.classList.toggle("srv-card-dark");
+
+    element = document.querySelector('.btn.secondary-btn');
+    element.classList.toggle("secondary-btn-dark");
+
+    element = document.querySelector('.logo');
+    element.classList.toggle("logo-dark");
+
+    element = document.querySelector('.nav-link');
+    element.classList.toggle("nav-link-dark");
+
+    element = document.querySelector('.btn');
+    element.classList.toggle("btn-dark");   
     
  }
+
+/*-------------------------------- Open & Close Navbar Menu -------------------------------*/
+
+hamburger.addEventListener("click", () => {
+    document.body.classList.toggle("open");
+    document.body.classList.toggle("stopscrolling");
+});
+
+links.forEach((link) =>
+    link.addEventListener("click", () => {
+        document.body.classList.remove("open");
+        document.body.classList.remove("stopscrolling");
+    })
+);
